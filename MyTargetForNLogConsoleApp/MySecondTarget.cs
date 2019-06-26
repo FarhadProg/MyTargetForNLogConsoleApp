b2156ebabc4fbee25a1f4b0f67396abf00d1acf2
zzeratul zzeratul <Talfar2019@outlook.com>
2019-06-26 09:13:02 +0300

    Фиксация 4  Добавление делигата для выбора метода для записи в Лог,  добавление concurentDictionary

---
+++ MyTargetForNLogConsoleApp/MySecondTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Concurrent;
+using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace MyTargetForNLogConsoleApp
+{
+    [Target("mySecond")]
+    class MySecondTarget:TargetWithLayout
+    {
+        public string fileName;
+
+
+    [RequiredParameter]
+        public string Host { get; set; }
+
+        public MySecondTarget()
+        {
+
+        }
+    }
+}
--- MyTargetForNLogConsoleApp/MyTarget.cs
+++ MyTargetForNLogConsoleApp/MyTarget.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
+using System.Collections.Concurrent;
 using System.IO;
 using NLog;
 using NLog.Config;
@@ -13,121 +14,90 @@ namespace MyTargetForNLogConsoleApp
     [Target("MyFirst")]
     public sealed class MyTarget : TargetWithContext
     {
-        private static readonly string BASE_LOG_DIR_NAME;
-        private static readonly string BASE_FILE_NAME;
-        private static readonly string FILE_EXTENSHION;
-        public string FullPath { get; private set; }
-        public string ThreadId { get; private set; }
-        public string ShortFileName { get; private set; }
+        private delegate void SelectWriteMethod(LogEventInfo logEvent);
+        private SelectWriteMethod selector;
+       
+        public ConcurrentDictionary<string, string> FilePathPatternCollection { get; set; }
 
- 
+        [RequiredParameter]
+        public string FilePathPattern { get; set; }
+        [RequiredParameter]
+        public string LogDirectory { get; set; }
+
+
+
 
         public MyTarget()
         {
+            if (FilePathPattern == null)
+                FilePathPattern = @"C:\LogsTMP\Log_#.log";
+            if(LogDirectory == null)
+                LogDirectory = @"C:\LogsTMP\";
+
+            FilePathPatternCollection = new ConcurrentDictionary<string, string>();
+          
+            IncludeEventProperties = true;          
             try
             {
-                IncludeEventProperties = true;
-                ThreadId = "0";
-                ShortFileName = "Log_0.log";
-                FullPath = Path.Combine(BASE_LOG_DIR_NAME, ShortFileName);
+                if (!Directory.Exists(LogDirectory))
+                {
+                    Directory.CreateDirectory(LogDirectory);
+                }
+               selector = this.WriteToLog;
+
             }
             catch (Exception ex)
             {
+                selector = this.DontWriteToLog;
                 Console.WriteLine("В конструктре класса произошла ошибка " + ex.Message);
             }
-        }
-        static MyTarget()
-        {
 
-            try
-            {
-                BASE_LOG_DIR_NAME = @"C:\LogsTMP\";
-                BASE_FILE_NAME = "Log_";
-                FILE_EXTENSHION = ".log";
-                if (!Directory.Exists(BASE_LOG_DIR_NAME))
-                {
-                    Directory.CreateDirectory(BASE_LOG_DIR_NAME);
-                }
-            }
-            catch (DriveNotFoundException ex)
-            {
-                Console.WriteLine("В статическом конструктре класса произошла ошибка " + ex.Message);
 
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine("В статическом конструктре класса произошла ошибка" + ex.Message);
-
-            }
-            catch (PathTooLongException ex)
-            {
-                Console.WriteLine("В статическом конструктре класса произошла ошибка " + ex.Message);
-
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine("В статическом конструктре класса произошла ошибка " + ex.Message);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("В статическом конструктре класса произошла ошибка " + ex.Message);
-            }
 
         }
+
+
 
 
         protected override void Write(LogEventInfo logEvent)
         {
+
+            selector(logEvent);
+        }
+
+        private void WriteToLog(LogEventInfo logEvent)
+        {
             try
             {
-                string logMessage = RenderLogEvent(Layout, logEvent);
                 IDictionary<string, object> logProperties = GetAllProperties(logEvent);
-                ThreadId = logProperties["@threadId"].ToString();
-                SetShortFileName();
-                FullPath = Path.Combine(BASE_LOG_DIR_NAME, ShortFileName);
-                using (StreamWriter sw = new StreamWriter(FullPath, true))
+                string threadId = logProperties["@threadId"].ToString();
+                string fullPathToFile = GetFileName(threadId);
+
+
+                string logMessage = RenderLogEvent(Layout, logEvent);
+                
+                using (StreamWriter sw = new StreamWriter(fullPathToFile, true))
                 {
                     sw.WriteLine(logMessage);
 
                 }
             }
-            catch (KeyNotFoundException ex)
-            {
-                Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
-            }
-            catch (DriveNotFoundException ex)
-            {
-                Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
-
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
-
-            }
-            catch (PathTooLongException ex)
-            {
-                Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
-
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
-
-            }
             catch (Exception ex)
             {
 
                 Console.WriteLine("При записи сообщения в лог произошла ошибка:" + ex.Message);
             }
-
         }
-
-        private void SetShortFileName()
+        private void DontWriteToLog(LogEventInfo logEvent)
         {
-            ShortFileName = BASE_FILE_NAME + ThreadId + FILE_EXTENSHION;
+
         }
+        private string  GetFileName(string threadID)
+        {
+            FilePathPatternCollection.TryAdd(threadID, FilePathPattern.Replace("#", threadID)); 
+            return (FilePathPatternCollection[threadID]);
+        }
+
 
     }
 }
